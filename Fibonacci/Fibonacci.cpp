8b9d6410c785066b4c8860aac6b92d33c4d3159a
Parncncd <69336494+Parncncd@users.noreply.github.com>
2020-08-16 23:45:20 +0700

    Add project files.

---
+++ Fibonacci/Fibonacci.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int i, A[10000], sum;
+	A[0] = { 1 };
+	A[1] = { 2 };
+	sum = 2;
+	for (i = 2; A[i] <= 1000000; i++) {
+		A[i] = A[i - 1] + A[i - 2];
+
+
+		if (A[i] % 2 == 0) {
+			sum += A[i];
+		}
+
+
+
+	}
+	printf("%d", sum);
+	return 0;
+
+}
