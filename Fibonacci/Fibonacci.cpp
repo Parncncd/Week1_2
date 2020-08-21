8c3a2b8951d9439f239c649110d8f52b63f158f3
Parncncd <69336494+Parncncd@users.noreply.github.com>
2020-08-21 23:41:27 +0700

    แก้เงื่อนไขในfor เป็น i-1

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -1,11 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main() {
-	int i, A[10000], sum;
+	int i, A[1000], sum;
 	A[0] = { 1 };
 	A[1] = { 2 };
 	sum = 2;
-	for (i = 2; A[i] <= 1000000; i++) {
+	for (i = 2; A[i-1] <= 4000000; i++) {
 		A[i] = A[i - 1] + A[i - 2];
 
 
